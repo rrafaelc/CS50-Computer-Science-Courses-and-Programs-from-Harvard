0c9868ce4f4d1a5ff2ba1677ffd8b13e221567e9
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-22 13:26:43 +0000

    Fri, Jul 22, 2022, 1:26 PM +00:00

---
+++ positive.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_positive_int(void);
+
+int main(void)
+{
+  int i = get_positive_int();
+  printf("%i\n", i);
+}
+
+int get_positive_int(void)
+{
+}
