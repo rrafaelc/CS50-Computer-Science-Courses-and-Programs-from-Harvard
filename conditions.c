d90ffdef680a53ead887b097748a3ec035be1470
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-22 00:47:53 +0000

    Fri, Jul 22, 2022, 12:47 AM +00:00

--- conditions.c
+++ conditions.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
-  
+int main(void)
+{
+  int x = get_int("x: ");
+
+  int y = get_int("y: ");
+
+  if (x < y)
+  {
+    printf("%i is less than %i\n", x, y);
+  }
+  else if (x > y)
+  {
+    printf("%i is greater than %i\n", x, y);
+  }
+  else
+  {
+    printf("%i is equal to %i\n", x, y);
+  }
 }
