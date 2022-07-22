7e1ecf60c5465389f2d4c100f21fabaed15016bb
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-22 15:19:25 +0000

    Fri, Jul 22, 2022, 3:19 PM +00:00

--- module1/exercises/mario_easy.c
+++ module1/exercises/mario_easy.c
@@ -2,10 +2,13 @@
 #include <cs50.h>
 
 int get_user_height(void);
+void pyramid(int height);
 
 int main(void)
 {
   int height = get_user_height();
+
+  pyramid(height);
 }
 
 int get_user_height(void)
@@ -17,4 +20,17 @@ int get_user_height(void)
     n = get_int("Height: ");
   } while (n < 1 || n > 8);
   return n;
+}
+
+void pyramid(int height)
+{
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = i; j > 1; j--)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
+  // printf("\n");
 }
