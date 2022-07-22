c37828284ecc21af70de6e2fa2e8366a63f5c1a7
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-22 00:50:07 +0000

    Fri, Jul 22, 2022, 12:50 AM +00:00

---
+++ agree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  char 
+
+  if (x < y)
+  {
+    printf("%i is less than %i\n", x, y);
+  }
+  else if (x > y)
+  {
+    printf("%i is greater than %i\n", x, y);
+  }
+  else
+  {
+    printf("%i is equal to %i\n", x, y);
+  }
+}
