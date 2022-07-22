ca01e13c59e7df5db80c5d10073b850e496bf26a
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-22 15:00:12 +0000

    Fri, Jul 22, 2022, 3:00 PM +00:00

--- module1/exercises/mario_easy.c
+++ module1/exercises/mario_easy.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_user_height(void);
+
 int main(void)
 {
-  
+  int height = get_user_height();
+}
+
+int get_user_height(void)
+{
+  int n;
+
+  do
+  {
+    n = get_int("Height: ");
+  } while (n < 1 || n > 8);
+  return n;
 }
