d2a130da472e421ab2dbaa36543ef9f7920f0013
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-26 17:08:53 +0000

    Tue, Jul 26, 2022, 5:08 PM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -3,7 +3,28 @@
 
 // AMEX, MASTERCARD, VISA or INVALID
 
+// American Express - 15 digits, starts with 34 or 37
+// Mastercard - 16 digits, starts with 51, 52, 53, 54 or 55
+// Visa - 13 or 16 digits, starts with 4
+
+long get_card_number();
+
 int main(void)
 {
+    // Get card number
+    long card = get_card_number();
+}
 
+long get_card_number() {
+    long card;
+
+    // do
+    // {
+        // Get card number from user
+        // do
+        // {
+            card = get_long("Number: ");
+            return card;
+        // } while
+    // }
 }
