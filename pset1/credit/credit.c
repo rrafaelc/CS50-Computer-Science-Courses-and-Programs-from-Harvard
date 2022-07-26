e0568c5d9cd7315443250549fa4f162bb5aefd84
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-26 20:43:10 +0000

    Tue, Jul 26, 2022, 8:43 PM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -37,58 +37,68 @@ long get_card_number()
 
 void validate_credit(long number)
 {
+    // Copy the number to keep intact, for later print the number to user;
     long num = number;
 
+    // Iniate variables for the operations
     int multipliedBy2 = 0;
     int notMultipliedBy2 = 0;
+
+    // Use count to keep track to get a the right number in do while
     int count = 1;
 
     do
     {
+        // Check if the count is even, to get the second-to-last digit
         if (count % 2 == 0)
         {
+            // Get the last digit
             int n = num % 10;
 
+            // Multiply by 2
             int multBy2 = n * 2;
 
-            // printf("%i\n", multBy2);
-
+            // If the result is higher than 9
             if (multBy2 > 9)
             {
+                // The number can't be greater than 18, so i know there will be only 2 numbers to sum
                 int firstNum = multBy2 % 10;
+
                 int secondNum = multBy2 / 10;
+
+                // Sum the numbers
                 int sum = firstNum + secondNum;
 
-                printf("%i\n", firstNum);
-                printf("%i\n", secondNum);
-                printf("%i\n", sum);
-
+                // Add to the multipliedBy2
                 multipliedBy2 += sum;
 
-            //     int sum = 0;
+                // And remove last digit from the num;
+                num /= 10;
 
-            //     do
-            //     {
-            //         sum += multBy2 % 10;
-            //         multBy2 /= 10;
-            //     }
-            //     while (multBy2 > 0);
+            } else {
+                // If not greater than 9, only sum
+                multipliedBy2 += multBy2;
 
-            //     multipliedBy2 += multBy2;
+                // And remove last digit again
+                num /= 10;
             }
 
-            multipliedBy2 += multBy2;
-
-            num /= 10;
-        } else
+        }
+        else
         {
-            notMultipliedBy2 = num % 10;
+            // Add last digit to the variable
+            notMultipliedBy2 += num % 10;
+
+            // Remove last digit
             num /= 10;
         }
 
         count++;
     }
+    // The result will reach 0 eventually dividing by 10
     while (num > 0);
+
+    notMultipliedBy2 += multipliedBy2;
 
     printf("\n");
     printf("By 2: %i\n", multipliedBy2);
