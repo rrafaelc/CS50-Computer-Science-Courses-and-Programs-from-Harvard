42ed20cf41ebd02858d7d682c0a603a509b5484e
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-27 12:57:59 +0000

    Wed, Jul 27, 2022, 12:57 PM +00:00

--- weeks/week2/length.c
+++ weeks/week2/length.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
-
-int string_length(string s);
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("Name: ");
-    int length = string_length(name);
+    int length = strlen(name);
     printf("%i\n", length);
-}
-
-int string_length(string s)
-{
-    int i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return i;
 }
