3730a700c46c17cdf2bb2f55b5bee776b6c92d26
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-27 12:15:40 +0000

    Wed, Jul 27, 2022, 12:15 PM +00:00

--- weeks/week2/scores.c
+++ weeks/week2/scores.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int scores[3];
 
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
