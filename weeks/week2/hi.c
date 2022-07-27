61b4b8038f47c46c3548b56e59e6c1ca8c37d9f2
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-27 12:29:23 +0000

    Wed, Jul 27, 2022, 12:29 PM +00:00

--- weeks/week2/hi.c
+++ weeks/week2/hi.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    printf("%i %i %i\n", (int) c1, (int) c2, (int) c3);
+    string s = "HI!";
+    printf("%s\n", s);
 }
