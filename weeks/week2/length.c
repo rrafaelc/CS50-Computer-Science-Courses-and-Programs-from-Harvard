8f0a12afc9e06502ce7861a9401281ca0e970f45
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-27 12:45:28 +0000

    Wed, Jul 27, 2022, 12:45 PM +00:00

---
+++ weeks/week2/length.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("Name: ");
+
+    int i = 0;
+
+    while (name[i] != '\0')
+    {
+        i++;
+    }
+
+    printf("%i\n", i);
+}
