3ef8d4dfc2ade3926ddf10145265872d62e4eb78
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 00:08:10 +0000

    Thu, Jul 28, 2022, 12:08 AM +00:00

--- psets/pset2/readability/readability.c
+++ psets/pset2/readability/readability.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #include <ctype.h>
 
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
 int main(void)
 {
-    //
+    // Get user input
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 }
+
+int count_letters(string text)
+{}
+
+int count_words(string text)
+{}
+
+int count_sentences(string text)
+{}
