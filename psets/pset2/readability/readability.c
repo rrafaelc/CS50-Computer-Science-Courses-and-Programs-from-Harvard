13890fc96a6894e4e3cfaf1e3d544a963fb44c8a
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 00:46:32 +0000

    Thu, Jul 28, 2022, 12:46 AM +00:00

--- psets/pset2/readability/readability.c
+++ psets/pset2/readability/readability.c
@@ -28,12 +28,12 @@ int main(void)
     float s = (float) sentences / words * 100;
 
     // Calculate using formula
-    // int readability = round(0.0588 * l - 0.296 * s - 15.8);
+    int readability = round(0.0588 * l - 0.296 * s - 15.8);
 
-    float readability = 0.0588 * l - 0.296 * s - 15.8;
+    // float readability = 0.0588 * l - 0.296 * s - 15.8;
 
 
-    printf("Grade %f\n",  readability);
+    printf("Grade %i\n",  readability);
 
 }
 
