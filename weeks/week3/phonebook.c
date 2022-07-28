8cb2c94beab636fced8c7100d135eb99ede262b4
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 17:30:59 +0000

    Thu, Jul 28, 2022, 5:30 PM +00:00

--- weeks/week3/phonebook.c
+++ weeks/week3/phonebook.c
@@ -5,7 +5,18 @@
 int main(void)
 {
     string names[] = {"Carter", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"}
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
-    return 0;
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], "David") == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+    return 1;
 }
