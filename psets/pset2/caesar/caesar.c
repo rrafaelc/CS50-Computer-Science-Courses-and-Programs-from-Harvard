4045fcfa127616c8565d70344452acc0eb7a2aa6
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 10:35:11 +0000

    Thu, Jul 28, 2022, 10:35 AM +00:00

--- psets/pset2/caesar/caesar.c
+++ psets/pset2/caesar/caesar.c
@@ -6,5 +6,13 @@
 
 int main(int argc, string argv[])
 {
+    if (argc == 1 || argc > 2)
+    {
+        // Check if has no command
+        printf("Usage: ./caesar key\n");
+
+        return 1;
+    }
+
     // printf("-> %i\n", (('Y' - 65) + 3) % 26);
 }
