22c749c1dcab194167c107b455fa9d56b3d83e36
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 15:09:13 +0000

    Thu, Jul 28, 2022, 3:09 PM +00:00

--- weeks/week3/names.c
+++ weeks/week3/names.c
@@ -1,9 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+    string names[] = {4, 6, 8, 2, 7, 5, 0};
 
     for (int i = 0; i < 7; i++)
     {
