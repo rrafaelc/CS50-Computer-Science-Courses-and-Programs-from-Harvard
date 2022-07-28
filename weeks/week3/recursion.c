d712ebbb8118ecad2f4a4a9b3e965e1be53ad3b7
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 22:01:35 +0000

    Thu, Jul 28, 2022, 10:01 PM +00:00

--- weeks/week3/recursion.c
+++ weeks/week3/recursion.c
@@ -12,14 +12,11 @@ int main(void)
 
 void draw(int n)
 {
+    draw(n - 1);
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        printf("#");
     }
-
+    printf("\n");
 }
