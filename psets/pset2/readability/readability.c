55c4692fb9ca5d84610f4b12f7c965e3ff904d5f
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 00:13:34 +0000

    Thu, Jul 28, 2022, 12:13 AM +00:00

--- psets/pset2/readability/readability.c
+++ psets/pset2/readability/readability.c
@@ -14,13 +14,35 @@ int main(void)
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
+
+    printf("Letters: %i\n", letters);
 }
 
 int count_letters(string text)
-{}
+{
+    int c_letters = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (islower(text[i]))
+        {
+            c_letters++;
+        }
+        else if (isupper(text[i]))
+        {
+            c_letters++;
+        }
+    }
+
+    return c_letters;
+}
 
 int count_words(string text)
-{}
+{
+    return 0;
+}
 
 int count_sentences(string text)
-{}
+{
+    return 0;
+}
