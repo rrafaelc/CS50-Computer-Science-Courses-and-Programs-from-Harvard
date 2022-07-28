9fee940138043034531496407b4ba324015b2502
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-28 21:52:41 +0000

    Thu, Jul 28, 2022, 9:52 PM +00:00

--- weeks/week3/iteration.c
+++ weeks/week3/iteration.c
@@ -1,9 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void draw(int n);
+
 int main(void)
 {
     int height = get_int("Height: ");
 
     draw(height);
+}
+
+void draw(int n)
+{
+    fot (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n)
+        printf("#");
+    }
+
+    printf("\n");
 }
