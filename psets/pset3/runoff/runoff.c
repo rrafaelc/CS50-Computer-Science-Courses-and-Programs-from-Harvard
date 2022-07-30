84f20b81e59d822742f6d7fe6285252c54e6a9a2
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-30 16:20:03 +0000

    Sat, Jul 30, 2022, 4:20 PM +00:00

--- psets/pset3/runoff/runoff.c
+++ psets/pset3/runoff/runoff.c
@@ -127,7 +127,11 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ()
+    }
+
     return false;
 }
 
