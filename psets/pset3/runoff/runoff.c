1ed5f6a163406609fccf63509a771f63247ec9c1
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-30 19:27:24 +0000

    Sat, Jul 30, 2022, 7:27 PM +00:00

--- psets/pset3/runoff/runoff.c
+++ psets/pset3/runoff/runoff.c
@@ -149,6 +149,8 @@ bool vote(int voter, int rank, string name)
         // Find the candidate name
         if (strcmp(candidates[i].name, name) == 0)
         {
+            // Check if 
+
             // [0][0] = 2
             // [Joe][rank1] = Charlie
             preferences[voter][rank] = i;
