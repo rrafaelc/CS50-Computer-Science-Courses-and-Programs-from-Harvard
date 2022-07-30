bb9494fd24b8f9008b9263a48fd214acf8987137
Rafael Costa <rafaelcosta8020@gmail.com>
2022-07-30 22:17:52 +0000

    Sat, Jul 30, 2022, 10:17 PM +00:00

--- psets/pset3/tideman/tideman.c
+++ psets/pset3/tideman/tideman.c
@@ -126,15 +126,11 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    printf("Candidates: ");
-
     for (int i = 0; i < candidate_count; i++)
     {
-        printf("%i ", ranks[i]);
-
         for (int j = 0; j < candidate_count; j++)
         {
-            preferences[i][j] += 
+            preferences[i][j] += ranks[j];
         }
     }
 
