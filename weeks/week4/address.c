ca8ab613d1247ee7bb302361e459f371880338c5
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 16:39:50 +0000

    Mon, Aug 1, 2022, 4:39 PM +00:00

--- weeks/week4/address.c
+++ weeks/week4/address.c
@@ -18,4 +18,11 @@ int main(void)
     // printf("%c\n", *(s + 2));
 
     int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+    printf("%i\n", numbers[0]);
+    printf("%i\n", numbers[1]);
+    printf("%i\n", numbers[2]);
+    printf("%i\n", numbers[3]);
+    printf("%i\n", numbers[4]);
+    printf("%i\n", numbers[5]);
+    printf("%i\n", numbers[6]);
 }
