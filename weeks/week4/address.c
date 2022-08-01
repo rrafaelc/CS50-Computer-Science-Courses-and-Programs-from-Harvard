56c071855793611a86e0f90fad0f6a7d8772a55c
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 15:15:35 +0000

    Mon, Aug 1, 2022, 3:15 PM +00:00

--- weeks/week4/address.c
+++ weeks/week4/address.c
@@ -6,9 +6,13 @@ int main(void)
     // int *p = &n;
     // printf("%p\n", p);
     // printf("%i\n", *p);
+    // printf("%p\n", p);
+    // printf("%p\n", s);
+    // char *p = &s[0];
 
     char *s = "HI!";
-    char *p = &s[0];
-    printf("%p\n", p);
-    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 }
