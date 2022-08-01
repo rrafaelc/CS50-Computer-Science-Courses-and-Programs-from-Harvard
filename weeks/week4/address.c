2e95d2f31f9a9722668dde0738d947b2589a4231
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 14:05:24 +0000

    Mon, Aug 1, 2022, 2:05 PM +00:00

--- weeks/week4/address.c
+++ weeks/week4/address.c
@@ -1,9 +1,13 @@
+// #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%p\n", p);
-    printf("%i\n", *p);
+    // int n = 50;
+    // int *p = &n;
+    // printf("%p\n", p);
+    // printf("%i\n", *p);
+
+    char *s = "HI!";
+    printf("%s\n", s);
 }
