88d1bc59f7da74c30d1230b45d53853b20fdf123
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 16:55:29 +0000

    Mon, Aug 1, 2022, 4:55 PM +00:00

--- weeks/week4/compare.c
+++ weeks/week4/compare.c
@@ -7,12 +7,6 @@ int main(void)
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    if (strcmp(s, t) == 0)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    printf("%s\n", s);
+    printf("%s\n", t);
 }
