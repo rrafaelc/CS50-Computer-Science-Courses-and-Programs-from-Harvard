5c5d8f9196e6a0c48b6b20d70c831a3a0703f6a9
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 23:39:34 +0000

    Mon, Aug 1, 2022, 11:39 PM +00:00

---
+++ weeks/week4/memory.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int *x = malloc(3 * sizeof(int));
+
+    // char *s = get_string("s: ");
+    x[0] = 72;
+    x[1] = 73;
+    x[2] = 33;
+
+    printf("");
+
+    return 0;
+}
