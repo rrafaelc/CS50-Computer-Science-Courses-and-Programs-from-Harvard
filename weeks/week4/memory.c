0c3f342bb8d0b88b42f65d408feb8d9fd4c0cefe
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 23:42:58 +0000

    Mon, Aug 1, 2022, 11:42 PM +00:00

--- weeks/week4/memory.c
+++ weeks/week4/memory.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cs50.h>
 
 int main(void)
 {
     int *x = malloc(3 * sizeof(int));
-
     x[0] = 72;
     x[1] = 73;
-    x[3] = 33;
-
-    printf("");
-
-    return 0;
+    x[2] = 33;
 }
