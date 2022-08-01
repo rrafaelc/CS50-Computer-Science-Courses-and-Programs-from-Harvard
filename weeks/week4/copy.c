8d323d400810b34bb810f553eb035eeaf0a7d3bd
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-01 17:15:58 +0000

    Mon, Aug 1, 2022, 5:15 PM +00:00

--- weeks/week4/copy.c
+++ weeks/week4/copy.c
@@ -9,13 +9,21 @@ int main(void)
     char *s = get_string("s: ");
 
     char *t = malloc(strlen(s) + 1);
+    if (t = NULL)
+    {
+        return 1;
+    }
 
     strcpy(t, s);
 
-    t[0] = toupper(t[0]);
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
 
     printf("s %s\n", s);
     printf("t %s\n", t);
 
     free(t);
+    return 0;
 }
