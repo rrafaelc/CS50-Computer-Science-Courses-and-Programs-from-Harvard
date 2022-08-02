d31cbccbeedacb70edf63fed656740b4c60e3ac4
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-02 01:22:17 +0000

    Tue, Aug 2, 2022, 1:22 AM +00:00

--- weeks/week4/scanf.c
+++ weeks/week4/scanf.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    int x;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x: %i\n", x);
+    char *s;
+    printf("s: ");
+    scanf("%s", &s);
+    printf("s: %i\n", s);
 }
