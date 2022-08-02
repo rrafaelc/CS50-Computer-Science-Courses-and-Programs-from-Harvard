4707b319e03d82d8b126e66298ad076800263a23
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-02 01:16:24 +0000

    Tue, Aug 2, 2022, 1:16 AM +00:00

--- weeks/week4/swap.c
+++ weeks/week4/swap.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void swap(int a, int b);
+void swap(int *a, int *b);
 
 int main(void)
 {
@@ -8,15 +8,13 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(x, y);
+    swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    printf("a is %i, b is %i\n", a, b);
-    int tmp = a;
-    a = b;
-    b = tmp;
-    printf("a is %i, b is %i\n", a, b);
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
