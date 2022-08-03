30ecf69787ca02114a2af6390e65b65d85365096
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 21:17:34 +0000

    Wed, Aug 3, 2022, 9:17 PM +00:00

--- psets/pset4/filter-less/helpers.c
+++ psets/pset4/filter-less/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // get last row of image image[height - 1]
 // image[0] first row
@@ -9,18 +10,30 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop the height
     for (int i = 0; i < height; i++)
     {
+        // Loop the width
         for (int j = 0; j < width; j++)
         {
+            // Get all 3 rgb colors
             int red = image[i][j].rgbtRed;
             int green = image[i][j].rgbtGreen;
             int blue = image[i][j].rgbtBlue;
-            int average = (red + green + blue) / 3;
 
-            *red = average;
-            *green = average;
-            *blue = average;
+            // Calculate the average and round
+            int average = round((red + green + blue) / 3);
+
+            // If the average is higher than 255 set to 255
+            if (average > 255)
+            {
+                average = 255;
+            }
+
+            // Set for each pixel the result of average
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
         }
     }
 
