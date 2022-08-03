7015885028f26c1496456a2f0137c52ced478050
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 20:50:53 +0000

    Wed, Aug 3, 2022, 8:50 PM +00:00

--- psets/pset4/filter-less/helpers.c
+++ psets/pset4/filter-less/helpers.c
@@ -13,9 +13,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = 0;
-            image[i][j].rgbtGreen = 0;
-            image[i][j].rgbtBlue = 0;
+            image[i][j].rgbtRed = 200;
+            image[i][j].rgbtGreen = 125;
+            image[i][j].rgbtBlue = 80;
         }
     }
 
