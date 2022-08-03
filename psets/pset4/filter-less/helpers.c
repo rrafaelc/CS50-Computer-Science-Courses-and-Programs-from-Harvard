cb67c95a705c2225a47cc665a212f3719aefdd86
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 23:12:02 +0000

    Wed, Aug 3, 2022, 11:12 PM +00:00

--- psets/pset4/filter-less/helpers.c
+++ psets/pset4/filter-less/helpers.c
@@ -148,7 +148,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int dTL[3], top[3], dTR[3], left[3], right[3], dBL[3], bottom[3], dBR[3];
             // Get the diagonal top left pixels
 
-            if (originalImage[row - 1][col - 1] < 0)
+            if (originalImage[-1][0] < 0)
             {
                 printf("Nao\n");
             }
