375daa0ea9faece4aa2f401598db0de01afad08a
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 18:32:32 +0000

    Wed, Aug 3, 2022, 6:32 PM +00:00

--- labs/volume/volume.c
+++ labs/volume/volume.c
@@ -34,7 +34,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[44];
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, input);
 
     // TODO: Read samples from input file and write updated data to output file
 
