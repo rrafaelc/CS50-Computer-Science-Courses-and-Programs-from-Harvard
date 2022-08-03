3bf76df9043894c4c30fc7be65e740b58d8853f2
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 18:33:21 +0000

    Wed, Aug 3, 2022, 6:33 PM +00:00

--- labs/volume/volume.c
+++ labs/volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
