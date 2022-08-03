ef8d66a96437b45d31ff518fededc794969a3bed
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-03 19:40:44 +0000

    Wed, Aug 3, 2022, 7:40 PM +00:00

--- psets/pset4/filter-less/helpers.c
+++ psets/pset4/filter-less/helpers.c
@@ -1,6 +1,10 @@
 #include "helpers.h"
 
 // get last row of image image[height - 1]
+// image[0] first row
+// image[0][0] first pixel in first row
+// image[0][1] second pixel in first row and so on
+// image[2][3] pixel in row 2 and column 3
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
