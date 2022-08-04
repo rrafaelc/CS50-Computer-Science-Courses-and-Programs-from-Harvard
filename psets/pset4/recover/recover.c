7c63d8fb0c59cf9e8018fc2c33c620d7650b46bc
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-04 15:04:01 +0000

    Thu, Aug 4, 2022, 3:04 PM +00:00

--- psets/pset4/recover/recover.c
+++ psets/pset4/recover/recover.c
@@ -22,5 +22,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    unsigned char buffer[3];
+
+    size_t ret = fread(buffer, 3, 3, f);
+
+    if (ret != sizeof(*buffer)) {
+        fprintf(stderr, "fread() failed: %zu\n", ret);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("ELF magic: %#04x%02x%02x%02x\n", buffer[0], buffer[1],
+           buffer[2]);
+
+
     fclose(f);
 }
