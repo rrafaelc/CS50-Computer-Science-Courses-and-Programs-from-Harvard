cd6422ef13b74f4d2a18bd2dac03e1e665cbaff7
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-04 14:44:15 +0000

    Thu, Aug 4, 2022, 2:44 PM +00:00

--- psets/pset4/recover/recover.c
+++ psets/pset4/recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Check the arguments
+    if (argc == 1 || argc > 2)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
 }
