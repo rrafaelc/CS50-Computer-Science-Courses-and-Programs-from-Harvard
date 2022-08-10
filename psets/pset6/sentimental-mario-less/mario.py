a924587e9487c84d0284828f4ddc6333640f61f3
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-10 23:52:39 +0000

    Wed, Aug 10, 2022, 11:52 PM +00:00

--- psets/pset6/sentimental-mario-less/mario.py
+++ psets/pset6/sentimental-mario-less/mario.py
@@ -1,15 +1,11 @@
 # Import the get int function from cs50
 from cs50 import get_int
 
-# Get the input from user
+# Get the input from user in range of 1 to 8
 while (True):
     height = get_int("Height: ")
-
-    if(1 <= height <= 8 ):
+    if(height in range(1, 9)):
         break
-
-
-
 
 # for i in range(height):
     # for j in range(height):
