8535e9d5ac89c6579de97df31fcf515b437c7a7e
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-10 23:42:49 +0000

    Wed, Aug 10, 2022, 11:42 PM +00:00

---
+++ psets/pset6/sentimental-mario-less/mario.py
@@ -0,0 +1,8 @@
+# Import the get string function from cs50
+from cs50 import get_string
+
+# Get the input from user
+height = get_string("Height ")
+
+# Print the hello, {name}
+print(f"hello, {name}")
