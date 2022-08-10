d57fa67bbcfe74c286b56f0afd6e22e2c86f9603
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-10 22:56:42 +0000

    Wed, Aug 10, 2022, 10:56 PM +00:00

--- labs/world-cup/tournament.py
+++ labs/world-cup/tournament.py
@@ -18,8 +18,10 @@ def main():
     # TODO: Read teams into memory from file
     with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
+
+        # Add the rows to the teams
         for row in reader:
-            print(row)
+            teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
