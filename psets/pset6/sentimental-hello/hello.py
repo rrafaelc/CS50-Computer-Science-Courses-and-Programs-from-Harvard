94ac0a7d0515c281355b3a8c3831832b5d2c58a4
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-10 23:40:57 +0000

    Wed, Aug 10, 2022, 11:40 PM +00:00

--- psets/pset6/sentimental-hello/hello.py
+++ psets/pset6/sentimental-hello/hello.py
@@ -1,2 +1,8 @@
-# TODO
-import 
+# Import the get string function from cs50
+from cs50 import get_string
+
+# Get the input from user
+name = get_string("What is your name? ")
+
+# Print the hello, {name}
+print(f"hello, {name}")
