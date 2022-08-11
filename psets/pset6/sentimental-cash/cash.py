3469b4baf11493648212a7feb3136084c93a54fe
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-11 00:14:45 +0000

    Thu, Aug 11, 2022, 12:14 AM +00:00

--- psets/pset6/sentimental-cash/cash.py
+++ psets/pset6/sentimental-cash/cash.py
@@ -26,7 +26,7 @@ def main():
     coins = quarters + dimes + nickels + pennies
 
     # Print total number of coins to give the customer
-    print(coins)
+    print(int(coins))
 
 
 def get_cents():
