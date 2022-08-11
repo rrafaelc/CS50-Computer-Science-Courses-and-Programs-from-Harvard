f8abb0616ad073aa0e3642fa1e5ea29b7c54d56d
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-11 00:08:33 +0000

    Thu, Aug 11, 2022, 12:08 AM +00:00

--- psets/pset6/sentimental-cash/cash.py
+++ psets/pset6/sentimental-cash/cash.py
@@ -1,78 +1,75 @@
-from cs50 import get_string
+from cs50 import get_int
 
 
 def main():
-{
+
     # Ask how many cents the customer is owed
-    int cents = get_cents();
+    cents = get_cents()
 
     # Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+    int quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
 
     # Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+    int dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
 
     # Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+    int nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
 
     # Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
+    int pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
 
     # Sum coins
-    int coins = quarters + dimes + nickels + pennies;
+    int coins = quarters + dimes + nickels + pennies
 
     # Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
+    printf("%i\n", coins)
 
-int get_cents(void)
-{
-    int cents;
 
+def get_cents():
     # Get amount of positive cents
     do
-    {
-        cents = get_int("Change Owed: ");
-    }
-    while (cents < 0);
 
-    return cents;
-}
+        cents = get_int("Change Owed: ")
 
-int calculate_quarters(int cents)
-{
+    while (cents < 0)
+
+    return cents
+
+
+def calculate_quarters(cents):
+
     # Divide the cents by quarter number, and the int return the integer number, not float
-    int quarters = cents / 25;
+    int quarters = cents / 25
 
-    return quarters;
-}
+    return quarters
 
-int calculate_dimes(int cents)
-{
+
+def calculate_dimes(cents):
+
     # Divide the cents by dimes number
-    int dimes = cents / 10;
+    int dimes = cents / 10
 
-    return dimes;
-}
+    return dimes
 
-int calculate_nickels(int cents)
-{
+
+def calculate_nickels(cents):
+
     # Divide the cents by nickels number
-    int nickels = cents / 5;
+    int nickels = cents / 5
 
-    return nickels;
-}
+    return nickels
 
-int calculate_pennies(int cents)
-{
+
+def calculate_pennies(cents):
+
     # Divide the cents by pennies number
-    int pennies = cents / 1;
+    int pennies = cents / 1
 
-    return pennies;
-}
+    return pennies
+
 
 main()
