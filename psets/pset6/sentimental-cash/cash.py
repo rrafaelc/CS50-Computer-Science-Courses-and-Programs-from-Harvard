edb86ec1d69036a51545a9e3238f8d26af59c3d6
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-11 14:49:47 +0000

    ddd, Aug 8/11/2022, YYYY, 2:49 A +0

--- psets/pset6/sentimental-cash/cash.py
+++ psets/pset6/sentimental-cash/cash.py
@@ -9,3 +9,18 @@ while(True):
 # Multiple the float to 100
 coins = int(cents * 100)
 
+# Get quarters coins
+quarters = coins // 25
+coins -= quarters * 25
+
+# Get dimes coins
+dimes = coins // 10
+coins -= dimes * 10
+
+# Get nickels coins
+nickels = coins // 5
+coins -= nickels * 5
+
+# Get pennies coins
+pennies = coins // 1
+coins -= pennies * 1
