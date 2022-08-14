9f5765f83d50bf1857284c4035adf0121b224c1b
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-14 23:46:22 +0000

    Sun, Aug 14, 2022, 11:46 PM +00:00

--- psets/pset7/movies/4.sql
+++ psets/pset7/movies/4.sql
@@ -2,4 +2,5 @@
 -- Your query should output a table with a single column and a single row (not counting the header) containing the number of movies with a 10.0 rating.
 
 
-SELECT title FROM movies, ratings
+SELECT title FROM ratings, movies
+WHERE movie_id = movies.id
