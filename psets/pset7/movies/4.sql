95749bfefc4fee1544dc3d1d57b5694cd39a0134
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-14 23:43:40 +0000

    Sun, Aug 14, 2022, 11:43 PM +00:00

--- psets/pset7/movies/4.sql
+++ psets/pset7/movies/4.sql
@@ -1,1 +1,5 @@
-SELECT name 
+-- write a SQL query to determine the number of movies with an IMDb rating of 10.0.
+-- Your query should output a table with a single column and a single row (not counting the header) containing the number of movies with a 10.0 rating.
+
+
+SELECT title FROM movies
