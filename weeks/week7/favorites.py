129c97e9201ef891434b464e2e09e1ea3e133cf2
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-14 12:57:03 +0000

    Sun, Aug 14, 2022, 12:57 PM +00:00

--- weeks/week7/favorites.py
+++ weeks/week7/favorites.py
@@ -7,11 +7,14 @@ db = SQL("sqlite:///favorites.db")
 title = input("Title: ").strip()
 
 # rows = db.execute("SELECT COUNT(*) AS counter FROM favorites WHERE title LIKE ?", title)
-rows = db.execute("SELECT title counter FROM favorites WHERE title LIKE ?", title)
+rows = db.execute("SELECT title FROM favorites WHERE title LIKE ?", title)
 
 # print(rows)
 # [{'counter': 4}]
+# [{'title': 'The Office'}, {'title': 'The Office'}, {'title': 'The Office'}]
 
-row = rows[0]
+# row = rows[0]
 
-print(row["counter"])
+# print(row["counter"])
+for row in rows:
+    print(row["title"])
