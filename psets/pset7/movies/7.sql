393469813812961b95860c5b7b4deec2ee6128f5
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 00:16:12 +0000

    Mon, Aug 15, 2022, 12:16 AM +00:00

--- psets/pset7/movies/7.sql
+++ psets/pset7/movies/7.sql
@@ -6,4 +6,4 @@
 
 SELECT movies.title, ratings.rating FROM movies JOIN ratings
 ON movies.id = ratings.movie_id
-WHERE year = 2010 ORDER BY ratings.rating, movies.title;
+WHERE year = 2010 ORDER BY ratings.rating DESC, movies.title;
