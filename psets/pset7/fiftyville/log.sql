9fac14ec6ad1dbea73e1a32e420e373541c676b9
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 15:42:44 +0000

    Mon, Aug 15, 2022, 3:42 PM +00:00

--- psets/pset7/fiftyville/log.sql
+++ psets/pset7/fiftyville/log.sql
@@ -49,4 +49,92 @@ WHERE flight_id = 36
 SELECT * FROM airports WHERE id = 4;
 -- New York City
 
--- Check phone numbers
+-- Check each name in phone call and receiver, and hope that i'm right :/
+
+-- airports              crime_scene_reports   people
+-- atm_transactions      flights               phone_calls
+-- bakery_security_logs  interviews
+-- bank_accounts         passengers
+
+-- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+-- Littering took place at 16:36. No known witnesses.
+
+-- +-----+------+-------+-----+------+--------+----------+---------------+
+-- | id  | year | month | day | hour | minute | activity | license_plate |
+-- +-----+------+-------+-----+------+--------+----------+---------------+
+-- | 459 | 2021 | 7     | 31  | 10   | 15     | exit     | 11J91FW       |
+-- +-----+------+-------+-----+------+--------+----------+---------------+
+
+-- +--------+------+----------------+-----------------+---------------+
+-- |   id   | name |  phone_number  | passport_number | license_plate |
+-- +--------+------+----------------+-----------------+---------------+
+-- | 375525 | Noah | (959) 555-4871 | 1095374669      | 11J91FW       |
+-- +--------+------+----------------+-----------------+---------------+
+
+-- | Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+-- | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
+
+-- +-----+----------------+------+-------+-----+----------------+------------------+--------+
+-- | id  | account_number | year | month | day |  atm_location  | transaction_type | amount |
+-- +-----+----------------+------+-------+-----+----------------+------------------+--------+
+-- | 246 | 28500762       | 2021 | 7     | 28  | Leggett Street | withdraw         | 48     |
+-- | 264 | 28296815       | 2021 | 7     | 28  | Leggett Street | withdraw         | 20     |
+-- | 266 | 76054385       | 2021 | 7     | 28  | Leggett Street | withdraw         | 60     |
+-- | 267 | 49610011       | 2021 | 7     | 28  | Leggett Street | withdraw         | 50     |
+-- | 269 | 16153065       | 2021 | 7     | 28  | Leggett Street | withdraw         | 80     |
+-- | 288 | 25506511       | 2021 | 7     | 28  | Leggett Street | withdraw         | 20     |
+-- | 313 | 81061156       | 2021 | 7     | 28  | Leggett Street | withdraw         | 30     |
+-- | 336 | 26013199       | 2021 | 7     | 28  | Leggett Street | withdraw         | 35     |
+-- +-----+----------------+------+-------+-----+----------------+------------------+--------+
+--                                                                                           |
+-- | Raymond | As the thief was leaving the bakery, he called someone who talked to him for less than a minute. In the call, I heard the thief say that he were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+
+-- +----+-------------------+------------------------+------+-------+-----+------+--------+
+-- | id | origin_airport_id | destination_airport_id | year | month | day | hour | minute |
+-- +----+-------------------+------------------------+------+-------+-----+------+--------+
+-- | 36 | 8                 | 4                      | 2021 | 7     | 29  | 8    | 20     |
+-- +----+-------------------+------------------------+------+-------+-----+------+--------+
+
+-- +-----------+-----------------+------+
+-- | flight_id | passport_number | seat |
+-- +-----------+-----------------+------+
+-- | 36        | 7214083635      | 2A   |
+-- | 36        | 1695452385      | 3B   |
+-- | 36        | 5773159633      | 4A   |
+-- | 36        | 1540955065      | 5C   |
+-- | 36        | 8294398571      | 6C   |
+-- | 36        | 1988161715      | 6D   |
+-- | 36        | 9878712108      | 7A   |
+-- | 36        | 8496433585      | 7B   |
+-- +-----------+-----------------+------+
+
+-- +--------+--------+----------------+-----------------+---------------+
+-- |   id   |  name  |  phone_number  | passport_number | license_plate |
+-- +--------+--------+----------------+-----------------+---------------+
+-- | 395717 | Kenny  | (826) 555-1652 | 9878712108      | 30G67EN       |
+-- | 398010 | Sofia  | (130) 555-0289 | 1695452385      | G412CB7       |
+-- | 449774 | Taylor | (286) 555-6063 | 1988161715      | 1106N58       |
+-- | 467400 | Luca   | (389) 555-5198 | 8496433585      | 4328GD8       |
+-- | 560886 | Kelsey | (499) 555-9472 | 8294398571      | 0NTHK55       |
+-- | 651714 | Edward | (328) 555-1152 | 1540955065      | 130LD9Z       |
+-- | 686048 | Bruce  | (367) 555-5533 | 5773159633      | 94KL13X       |
+-- | 953679 | Doris  | (066) 555-9701 | 7214083635      | M51FA04       |
+-- +--------+--------+----------------+-----------------+---------------+
+
+-- | Kiana   | I saw Richard take a bite out of his pastry at the bakery before his pastry was stolen from him.
+
+
+-- +-----+----------------+----------------+------+-------+-----+----------+
+-- | id  |     caller     |    receiver    | year | month | day | duration |
+-- +-----+----------------+----------------+------+-------+-----+----------+
+-- | 221 | (130) 555-0289 | (996) 555-8899 | 2021 | 7     | 28  | 51       |
+-- | 224 | (499) 555-9472 | (892) 555-8872 | 2021 | 7     | 28  | 36       |
+-- | 233 | (367) 555-5533 | (375) 555-8161 | 2021 | 7     | 28  | 45       |
+-- | 234 | (609) 555-5876 | (389) 555-5198 | 2021 | 7     | 28  | 60       |
+-- | 251 | (499) 555-9472 | (717) 555-1342 | 2021 | 7     | 28  | 50       |
+-- | 254 | (286) 555-6063 | (676) 555-6554 | 2021 | 7     | 28  | 43       |
+-- | 255 | (770) 555-1861 | (725) 555-3243 | 2021 | 7     | 28  | 49       |
+-- | 261 | (031) 555-6622 | (910) 555-3251 | 2021 | 7     | 28  | 38       |
+-- | 279 | (826) 555-1652 | (066) 555-9701 | 2021 | 7     | 28  | 55       |
+-- | 281 | (338) 555-6650 | (704) 555-2131 | 2021 | 7     | 28  | 54       |
+-- +-----+----------------+----------------+------+-------+-----+----------+
