5e665642307906bcb58dea45224fa7cb958d4560
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 12:12:24 +0000

    Mon, Aug 15, 2022, 12:12 PM +00:00

--- psets/pset7/fiftyville/log.sql
+++ psets/pset7/fiftyville/log.sql
@@ -1,3 +1,3 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- Theft took place on July 28
+-- Theft took place on July 28 || 7/28
 -- Theft took place on Humphrey Street
