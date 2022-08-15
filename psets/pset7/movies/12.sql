cb2678be368cef4b17f46c3fce9e8df2caddfafe
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:34:50 +0000

    Mon, Aug 15, 2022, 11:34 AM +00:00

---
+++ psets/pset7/movies/12.sql
@@ -0,0 +1,7 @@
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+
+
+
+JOIN people ON people.id = stars.person_id
+WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham"
