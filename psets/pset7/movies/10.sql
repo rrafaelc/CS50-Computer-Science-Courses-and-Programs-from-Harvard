40ac67ff64775cb589142ab01ed9e07051546f08
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:19:53 +0000

    Mon, Aug 15, 2022, 11:19 AM +00:00

--- psets/pset7/movies/10.sql
+++ psets/pset7/movies/10.sql
@@ -1,3 +1,7 @@
 SELECT DISTINCT(name) FROM people
 JOIN directors ON directors.person_id = people.id
-JOIN 
+
+
+JOIN movies ON directors.movie_id = movies.id
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE 
