4d58bdd544b525e2fa09ede572624b5289462417
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 12:17:39 +0000

    Mon, Aug 15, 2022, 12:17 PM +00:00

--- psets/pset7/fiftyville/log.sql
+++ psets/pset7/fiftyville/log.sql
@@ -1,3 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- Theft took place on July 28 || 7/28
 -- Theft took place on Humphrey Street
+
+-- Get the description from crime scene
+SELECT description FROM crime_scene_reports
+WHERE month = 7 AND day = 28
+AND street = "Humphrey Street"
