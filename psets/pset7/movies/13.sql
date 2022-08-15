98cf787e34764c0c788f06a4ed3dd9ea11dccbc5
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:55:51 +0000

    Mon, Aug 15, 2022, 11:55 AM +00:00

--- psets/pset7/movies/13.sql
+++ psets/pset7/movies/13.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT(name) FROM people
+SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 
 
@@ -7,7 +7,7 @@ JOIN movies ON stars.movie_id = movies.id
 WHERE movies.title
 
 IN (
-SELECT distinct(title) FROM movies
+SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Kevin Bacon" AND people.birth = 1958
