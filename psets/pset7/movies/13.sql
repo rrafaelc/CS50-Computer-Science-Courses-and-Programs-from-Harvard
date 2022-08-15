364cc0ea879ce808db6d6b09d1db7eb7ae165c05
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:52:50 +0000

    Mon, Aug 15, 2022, 11:52 AM +00:00

--- psets/pset7/movies/13.sql
+++ psets/pset7/movies/13.sql
@@ -1,12 +1,12 @@
-SELECT name FROM people
--- JOIN stars ON people.id = stars.person_id
+SELECT DISTINCT(name) FROM people
+JOIN stars ON people.id = stars.person_id
 
 
+JOIN movies ON stars.movie_id = movies.id
 
-
-WHERE name = "Kevin Bacon"
+WHERE movies.title
 IN (
-SELECT id FROM movies
+SELECT distinct(title) FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Helena Bonham Carter"
