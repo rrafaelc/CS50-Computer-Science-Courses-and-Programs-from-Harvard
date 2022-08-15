b424325b28539a15c3ab8e28fa11342bf321208f
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:40:58 +0000

    Mon, Aug 15, 2022, 11:40 AM +00:00

--- psets/pset7/movies/12.sql
+++ psets/pset7/movies/12.sql
@@ -4,4 +4,12 @@ JOIN stars ON movies.id = stars.movie_id
 
 
 JOIN people ON people.id = stars.person_id
-WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham"
+WHERE people.name = "Johnny Depp"
+
+AND title IN (
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id
+WHERE people.name = "Helena Bonham Carter"
+)
+
