f27bb8ca4990cf50974f0f462d9e72a33d804102
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-15 11:31:49 +0000

    Mon, Aug 15, 2022, 11:31 AM +00:00

--- psets/pset7/movies/11.sql
+++ psets/pset7/movies/11.sql
@@ -6,4 +6,4 @@ JOIN stars ON movies.id = stars.movie_id
 JOIN ratings ON ratings.movie_id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = "Chadwick Boseman"
-ORDER BY ratings.rating LIMIT 5
+ORDER BY ratings.rating DESC LIMIT 5
