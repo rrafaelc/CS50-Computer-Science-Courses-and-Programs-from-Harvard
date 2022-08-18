690f524b1abda3f2f96fd21d74ca99a315b7aec4
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-18 12:19:13 +0000

    Thu, Aug 18, 2022, 12:19 PM +00:00

--- weeks/week9/app.py
+++ weeks/week9/app.py
@@ -9,7 +9,4 @@ def index():
 @app.route("/greet", methods=["POST"])
 def greet():
   name = request.args.get("name", "world")
-  if(name == ""):
-    name = "world"
-
   return render_template("greet.html", name=name)
