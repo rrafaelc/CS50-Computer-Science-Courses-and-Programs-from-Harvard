d6a74c84b96aa885e090539522850a92b49c2871
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-18 11:28:33 +0000

    Thu, Aug 18, 2022, 11:28 AM +00:00

--- weeks/week9/app.py
+++ weeks/week9/app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+  name = request.args.get("name")
+  return render_template("greet.html", name=name)
