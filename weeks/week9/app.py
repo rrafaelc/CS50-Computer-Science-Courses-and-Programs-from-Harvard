72c90f0a417149be58a00ef89392aea0a85c1cdb
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-18 11:11:40 +0000

    Thu, Aug 18, 2022, 11:11 AM +00:00

--- weeks/week9/app.py
+++ weeks/week9/app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Live reload
-app.run(debug=True)
-
 @app.route("/")
 def index():
   return render_template("index.html")
