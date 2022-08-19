b1967c22bba3063cd8bfbcf60282cad30ba2f767
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-19 15:22:47 +0000

    Fri, Aug 19, 2022, 3:22 PM +00:00

--- labs/birthdays/app.py
+++ labs/birthdays/app.py
@@ -26,8 +26,11 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
