5b2af0dd34bf000d70d10bb37c11bd0ba869e798
Rafael Costa <rafaelcosta8020@gmail.com>
2022-08-21 00:10:57 +0000

    Sun, Aug 21, 2022, 12:10 AM +00:00

--- psets/pset9/finance/app.py
+++ psets/pset9/finance/app.py
@@ -92,9 +92,9 @@ def buy():
             row = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
 
             # Discount cash from total
-            row["cash"] -= int(stock[0]["total"])
-            total = usd(row["cash"] + int(stock[0]["total"]))
-            cash = usd(row["cash"])
+            cash = row["cash"] - int(stock[0]["total"])
+            total = usd(cash + int(stock[0]["total"]))
+            cash = usd(cash)
 
             # Convert to usd
             stock[0]["price"] = usd(stock[0]["price"])
